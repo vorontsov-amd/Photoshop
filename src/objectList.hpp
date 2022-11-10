46a9dea29255e3af07c74263f9c1555ae14c0a3d
Amadey Vorontsov <vorontsov.aa@phystech.edu>
2022-11-11 01:27:55 +0300

    something do it. I forgot

--- src/objectList.hpp
+++ src/objectList.hpp
@@ -2,7 +2,17 @@
 #include "widgetManager.hpp"
 #include "button.hpp"
 
-class ObjectList : public WidgetManager
+
+struct ObjInfo
+{
+    Vector position;
+    unsigned width;
+    unsigned height;
+    int num_widgets = 0;
+};
+
+
+class WindowBody : public WidgetManager
 {
 private:
     unsigned class_panel_size = 15;
@@ -13,8 +23,12 @@ private:
 
     Panel class_panel;
 
+    ObjInfo class_info;
+
 public:
-    ObjectList(const Vector& position, unsigned width, unsigned height) :
+    WindowBody(const ObjInfo& info) : WindowBody(info.position, info.width, info.height) {}
+    WindowBody(const Vector& position, unsigned width, unsigned height) :
+        class_info{position, width, height},
         class_panel{position, width, class_panel_size, class_panel_color} {
         double koef_button_size = 0.7;
         WidgetManager::addWidget(make_shared<TextureButton>(
@@ -30,17 +44,7 @@ public:
             sf::Color{100, 100, 100}
         ));
 
-        auto text = make_shared<TextButton>(
-            position + Vector{0, class_panel_size + height - 25}, 
-            width,
-            25,
-            "New Object",
-            sf::Color{80, 80, 80}
-        );
-        text->setTextColor(sf::Color::White);
-        text->moveText(120, 0);
-
-        WidgetManager::addWidget(text);
+        class_info.num_widgets = WidgetManager::arrayOfWidgets().size();
     }
 
     void draw(sf::RenderWindow& window) const override {
@@ -73,4 +77,68 @@ public:
         }  
         WidgetManager::pressButton(position);
     } 
+
+    ObjInfo info() { return class_info; }
 };
+
+
+class ObjectList;
+
+
+
+class ObjectCutaway : public WindowBody 
+{
+private:
+    ObjectList* class_obj_list;
+
+    static ObjInfo createBodyInfo(const ObjInfo& list_info) {
+        ObjInfo ret = {
+            list_info.position + Vector{20, 20},
+            list_info.width - 40,
+            list_info.height - 40,
+        };
+        return ret;
+    }
+public:
+    ObjectCutaway(ObjectList* obj_list);
+        //WindowBody::addWidget()
+};
+
+
+
+
+class ObjectList : public WindowBody
+{
+private:
+    sf::Color class_panel_color = sf::Color{190, 190, 190};
+
+    unsigned class_panel_size = 25;
+
+    sf::Vector2i class_last_position;
+    sf::Vector2i class_delta;
+public:
+    ObjectList(const Vector& position, unsigned width, unsigned height) : WindowBody{position, width, height} {
+        auto text = make_shared<TextButton>(
+            position + Vector{0, class_panel_size + height - 25}, 
+            width,
+            25,
+            "New Object",
+            sf::Color{80, 80, 80}
+        );
+
+        text->setTextColor(sf::Color::White);
+        text->moveText(120, 0);
+
+        WindowBody::addWidget(text);
+        WindowBody::addWidget(make_shared<ObjectCutaway>(this));
+    }
+
+    void draw(sf::RenderWindow& window) const override {
+        WindowBody::draw(window);
+    }
+
+};
+
+ObjectCutaway::ObjectCutaway(ObjectList* obj_list) : WindowBody{createBodyInfo(obj_list->info())}, class_obj_list{obj_list} {
+        //WindowBody::addWidget()
+    }
